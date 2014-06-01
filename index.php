beaa65f4be66d815f4de16df6cd063dbbd86f5c6
Michael Chandler <michael@chandler.id.au>
2014-06-01 15:18:39 +1000

    Fixing typos.

--- index.php
+++ index.php
@@ -7,12 +7,12 @@
     
     <title>php-calendar-template</title>
     
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">    
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">    
   </head>
   <body>
-    <?php require('repeat_partial.php'; ?>
+    <?php require_once('repeat_partial.php'); ?>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
   </body>
 </html>
