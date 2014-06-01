e169d35ec08b030955792501fc7379b6b6a2bb82
Michael Chandler <michael@chandler.id.au>
2014-06-01 19:58:52 +1000

    Missed php header.

--- db.php
+++ db.php
@@ -1,16 +1,18 @@
-$host = "us-cdbr-azure-west-a.cloudapp.net";
-$user = "b06ad6f8f7d08b";
-$pwd = "86682b84";
-$db = "phpcaleAvsfwvgD7";
+<?php
+   $host = "us-cdbr-azure-west-a.cloudapp.net";
+   $user = "b06ad6f8f7d08b";
+   $pwd = "86682b84";
+   $db = "phpcaleAvsfwvgD7";
 
-function createConnection() {
-   try {
-       $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pwd);
-       $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+   function createConnection() {
+      try {
+          $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pwd);
+          $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 
-       return $conn;
+          return $conn;
+      }
+      catch(Exception $e){
+          die(var_dump($e));
+      }
    }
-   catch(Exception $e){
-       die(var_dump($e));
-   }
-}
+?>
