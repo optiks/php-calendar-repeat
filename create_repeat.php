4a832b20e440546748dcd1c821ab9c30741701ed
Michael Chandler <michael@chandler.id.au>
2014-06-01 19:38:41 +1000

    Typo.

--- create_repeat.php
+++ create_repeat.php
@@ -15,28 +15,28 @@
         $user_id = 1; // TOOO: Get this from the session
         $duration_id = 1;
         $repeat_interval = 2;
-        $repeat_on_monday = true;
-        $repeat_on_tuesday = false;
-        $repeat_on_wednesday = true; 
-        $repeat_on_thursday = true;
-        $repeat_on_friday = true;
-        $repeat_on_saturday = false;
-        $repeat_on_sunday = false;
+        $repeat_on_monday = 1;
+        $repeat_on_tuesday = 0;
+        $repeat_on_wednesday = 1; 
+        $repeat_on_thursday = 1;
+        $repeat_on_friday = 1;
+        $repeat_on_saturday = 0;
+        $repeat_on_sunday = 1;
         $starts_on = "2013-04-01";
 
         $sql_insert = "INSERT INTO `repeat` (user_id, duration_id, repeat_interval, repeat_on_monday, repeat_on_tuesday, repeat_on_wednesday, repeat_on_thursday, repeat_on_friday, repeat_on_saturday, repeat_on_sunday, starts_on) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
         $stmt = $conn->prepare($sql_insert);
-        $stmt->bindValue(1, $user_id);
-        $stmt->bindValue(2, $duration_id);
-        $stmt->bindValue(3, $repeat_interval);
-        $stmt->bindValue(4, $repeat_on_monday);
-        $stmt->bindValue(5, $repeat_on_tuesday);
-        $stmt->bindValue(6, $repeat_on_wednesday);
-        $stmt->bindValue(7, $repeat_on_thursday);
-        $stmt->bindValue(8, $repeat_on_friday);
-        $stmt->bindValue(9, $repeat_on_saturday);
-        $stmt->bindValue(10, $repeat_on_sundaye);
-        $stmt->bindValue(11, $starts_on);
+        $stmt->bindValue(1, $user_id, PDO::PARAM_INT);
+        $stmt->bindValue(2, $duration_id, PDO::PARAM_INT);
+        $stmt->bindValue(3, $repeat_interval, PDO::PARAM_BOOL);
+        $stmt->bindValue(4, $repeat_on_monday, PDO::PARAM_BOOL);
+        $stmt->bindValue(5, $repeat_on_tuesday, PDO::PARAM_BOOL);
+        $stmt->bindValue(6, $repeat_on_wednesday, PDO::PARAM_BOOL);
+        $stmt->bindValue(7, $repeat_on_thursday, PDO::PARAM_BOOL);
+        $stmt->bindValue(8, $repeat_on_friday, PDO::PARAM_BOOL);
+        $stmt->bindValue(9, $repeat_on_saturday, PDO::PARAM_BOOL);
+        $stmt->bindValue(10, $repeat_on_sunday, PDO::PARAM_BOOL);
+        $stmt->bindValue(11, $starts_on, PDO::PARAM_DATE);
         $stmt->execute();
     }
     catch(Exception $e) {
