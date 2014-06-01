ac314ca44794326933fcabdfaf63d924ea9a0bdc
Michael Chandler <michael@chandler.id.au>
2014-06-01 20:03:56 +1000

    Removed db - dunno about PHP...

--- create_repeat.php
+++ create_repeat.php
@@ -1,6 +1,18 @@
 <?php
-   include_once('db.php');
-   $conn = createConnection();
+   $host = "us-cdbr-azure-west-a.cloudapp.net";
+   $user = "b06ad6f8f7d08b";
+   $pwd = "86682b84";
+   $db = "phpcaleAvsfwvgD7";
+
+   try {
+       $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pwd);
+       $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+       return $conn;
+   }
+   catch(Exception $e){
+       die(var_dump($e));
+   }
 
    try {
         $user_id = 1; // TOOO: Get this from the session
