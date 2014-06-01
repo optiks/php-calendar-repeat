2ac69af100a8831e32fbbf56761b609f65a8218a
Michael Chandler <michael@chandler.id.au>
2014-06-01 20:08:12 +1000

    Derp.

--- create_repeat.php
+++ create_repeat.php
@@ -7,8 +7,6 @@
    try {
        $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pwd);
        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-
-       return $conn;
    }
    catch(Exception $e){
        die(var_dump($e));
